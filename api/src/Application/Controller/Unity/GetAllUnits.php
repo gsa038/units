d86c26352c7dd2f07a87675f3b0544fddb132e50
Stanislav Gnoyanko <gsa038@mail.ru>
2020-12-16 14:59:55 +0300

    upd

--- api/src/Application/Controller/Unity/GetAllUnits.php
+++ api/src/Application/Controller/Unity/GetAllUnits.php
@@ -11,6 +11,12 @@ class GetAllUnits extends UnityController
     protected function action(ServerRequestInterface $request, Response $response): Response
     {
         $units = $this->unityRepository->getAllUnits();
+
+        if (count($units) > 0) {
+            $this->logger->info("Units was returned.");
+        } else {
+            $this->logger->info("There are no units.");
+        }
         return $this->respondWithData($units);
     }
 }
